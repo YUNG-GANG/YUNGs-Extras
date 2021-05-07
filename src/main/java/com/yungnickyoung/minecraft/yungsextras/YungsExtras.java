a60eaadbc9de0bda33bdc1681d3504aaa1852d43
Nicholas Young <ndy14@ufl.edu>
2021-05-06 23:45:10 -0400

    Initial commit.

---
+++ src/main/java/com/yungnickyoung/minecraft/yungsextras/YungsExtras.java
@@ -0,0 +1,21 @@
+package com.yungnickyoung.minecraft.yungsextras;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+
+@Mod(YungsExtras.MOD_ID)
+public class YungsExtras {
+    public static final String MOD_ID = "yungsextras";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    public YungsExtras() {
+        init();
+    }
+
+    private void init() {
+
+    }
+}
