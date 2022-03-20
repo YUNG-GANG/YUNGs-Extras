3a63647d214f4d46a04bda6c23dc7fb182cfdbc6
Nick Young <yungnickyoung@gmail.com>
2022-03-20 17:29:20 -0400

    v2.0. Removed old src dir.

--- src/main/java/com/yungnickyoung/minecraft/yungsextras/util/NamedConfiguredFeature.java
+++
@@ -1,5 +0,0 @@
-package com.yungnickyoung.minecraft.yungsextras.util;
-
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-
-public record NamedConfiguredFeature(String name, ConfiguredFeature<?, ?> configuredFeature) {}
